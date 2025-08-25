b4594b58e8dd70534ac224d89e2533e315fbffcc
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2025-08-25 17:47:15 +0530

    mildware setups

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import express from 'express'
 import connectDb from './db/index.js'
 import dotenv from 'dotenv'
+import {app} from './app.js'
 
 dotenv.config({ path: './env' })
-
-const app = express()
 
 connectDb()
 .then( ()=>{
