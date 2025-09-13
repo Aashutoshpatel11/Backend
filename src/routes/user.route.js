19f12a165d4c23ebc4091283301abc4b960b4373
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2025-09-13 20:15:23 +0530

    Add refresh token, change password, current user, and account update routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -22,5 +22,31 @@ userRoute.route("/login").post(loginUser)
 
 // secure route
 userRoute.route("/logout").post(verifyJWT, logoutUser)
+userRoute.route("/refresh-token").post(verifyJWT, refreshAccessToken)
+userRoute.route("/changeCurrentPassword").patch(verifyJWT, changeCurrentPassword)
+userRoute.route("/current-user").get(verifyJWT, getCurrentUser)
+userRoute.route("/update-account").patch(verifyJWT, updateAccountDetails)
+userRoute.route("/avatar").patch(
+    verifyJWT,
+    upload.single([
+        {
+            name: "avatar",
+            maxcount: 1
+        }
+    ]),
+    updateUserAvatar
+)
+userRoute.route("/cover-image").patch(
+    verifyJWT,
+    upload.single([
+        {
+            name: "coverImage",
+            maxcount: 1
+        }
+    ]) ,
+    updateCoverImage
+)
+userRoute.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+userRoute.route("/watch-history").get(verifyJWT, getUserWatchHistory)
 
 export {userRoute}
