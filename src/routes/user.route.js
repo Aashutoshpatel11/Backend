ff3e9f003a49139dfc7852e7480608c7503b3b9e
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2025-09-13 20:23:32 +0530

    missing imports

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getUserWatchHistory   
+
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,27 +39,9 @@ userRoute.route("/refresh-token").post(verifyJWT, refreshAccessToken)
 userRoute.route("/changeCurrentPassword").patch(verifyJWT, changeCurrentPassword)
 userRoute.route("/current-user").get(verifyJWT, getCurrentUser)
 userRoute.route("/update-account").patch(verifyJWT, updateAccountDetails)
-userRoute.route("/avatar").patch(
-    verifyJWT,
-    upload.single([
-        {
-            name: "avatar",
-            maxcount: 1
-        }
-    ]),
-    updateUserAvatar
-)
-userRoute.route("/cover-image").patch(
-    verifyJWT,
-    upload.single([
-        {
-            name: "coverImage",
-            maxcount: 1
-        }
-    ]) ,
-    updateCoverImage
-)
+userRoute.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+userRoute.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateCoverImage)
 userRoute.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 userRoute.route("/watch-history").get(verifyJWT, getUserWatchHistory)
-
+ 
 export {userRoute}
