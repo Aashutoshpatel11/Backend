7555d201cf2bd8ea8c7c0aa864e2b6a0d47862ff
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2025-11-15 09:18:07 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+function Navbar() {
+
+    const navbarElements = [
+        {
+            name: 'Home',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
+        },
+        {
+            name: 'Liked Videos',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
+        },
+        {
+            name: 'History',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M12 8v4l3 3"></path><circle cx="12" cy="12" r="10"></circle></svg>
+        },
+        {
+            name: 'My Content',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 7h16a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2z"></path><path d="M16 3l-4 4l-4-4"></path></svg>
+        },
+        {
+            name: 'Collections',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z"></path><path d="M8 4v16"></path></svg>
+        },
+        {
+            name: 'My Subscribers',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><circle cx="12" cy="7" r="4"></circle><path d="M5.5 21h13a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2z"></path></svg>
+        }
+    ]
+
+  return (
+    <div className="drawer lg:drawer-open">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+            {/* Navbar */}
+            <nav className="navbar w-full bg-base-300">
+            <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                {/* Sidebar toggle icon */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+            </label>
+            {/* <div className="px-4">Navbar Title</div> */}
+            <Header />
+            </nav>
+            {/* Page content here */}
+            {/* <div className="p-4">Page Content</div> */}
+        </div>
+
+        <div className="drawer-side is-drawer-close:overflow-visible">
+            <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+            <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
+            {/* Sidebar content here */}
+            <ul className="menu w-full grow">
+
+                {/* List item */}
+                {
+                    navbarElements.map( (element) => (
+                        <li key={element.name} >
+                            <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip={element.name}>
+                                {element.icon}
+                                <span className="is-drawer-close:hidden">{element.name}</span>
+                            </button>
+                        </li>
+                    ) )
+                }
+            </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
